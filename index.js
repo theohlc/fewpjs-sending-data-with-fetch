6edc7f533ce640c8f3ade2bde8bb9c4973fb1aab
theo <teddyhoover@gmail.com>
2021-03-24 12:21:52 -0500

    commit tree 1b0b796f25ec23482d96e6ca98c9ad71cd279601

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    const data = {
+        name:   name,
+        email:  email 
+    }
+    
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type":"application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(object) {
+            document.body.innerHTML = object["id"];
+        })
+        .catch(function(error) {
+            document.body.innerHTML = error.message
+        })
+}
